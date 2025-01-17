6ca52830387fbbcca53ff0467e31b1c3f4ccdde3
Devin Lane <devin.c.lane@gmail.com>
2025-01-17 12:51:30 -0800

    initial design with wider chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { MainChart } from "./components/main-chart";
 
 function App() {
     return (
-        <>
-            <h1>Long Covid DALYs</h1>
-            <MainChart />
-        </>
+        <div className="min-h-screen flex flex-col">
+            <h1 className="pb-4 md:pb-6 lg:pb-10 md:pt-2 lg:pt-4">
+                Long Covid DALYs
+            </h1>
+            <div className="">
+                <MainChart />
+            </div>
+        </div>
     );
 }
 
