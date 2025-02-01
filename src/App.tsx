33ab44aa0dd723770e45855997e6e849fcf2795c
Devin Lane <devin.c.lane@gmail.com>
2025-01-31 17:04:55 -0800

    add alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import { MainChart } from "./components/main-chart";
 import { SiteFooter } from "./components/site-footer";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 function App() {
+    const date = new Date().toLocaleDateString();
     return (
         <div className="min-h-screen flex flex-col">
+            <Alert className="bg-orange-100">
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    This is a work in progress and not currently displaying
+                    accurate data as of {date}
+                </AlertDescription>
+            </Alert>
+
             <header className="pb-4 md:pb-6 lg:pb-10 md:pt-2 lg:pt-4">
                 <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
                     Long Covid DALYs
