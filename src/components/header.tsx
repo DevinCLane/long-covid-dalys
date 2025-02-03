8de87cf9583cdb5cabb8601bfb47f1512af0a871
Devin Lane <devin.c.lane@gmail.com>
2025-02-03 12:44:10 -0800

    🧹 cleanup: split out some components

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+    return (
+        <header className="pb-4 md:pb-6 lg:pb-10 md:pt-2 lg:pt-4">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Long Covid DALYs
+            </h1>
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+                How public health interventions could decrease or increase cases
+                of long covid
+            </p>
+        </header>
+    );
+}
