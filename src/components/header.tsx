33ecdf4b5d69f3b1b1cb6ff57a21fec3e366b1f9
Devin Lane <devin.c.lane@gmail.com>
2025-05-16 14:55:54 -0700

    :speech_balloon: update header text

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,13 @@
 export function Header() {
-    return (
-        <header className="pb-4 md:pb-6 lg:pb-10 md:pt-2 lg:pt-4">
-            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                Long Covid and Public Health
-            </h1>
-            <p className="leading-7 [&:not(:first-child)]:mt-6">
-                How public health interventions could decrease or increase cases
-                of long covid
-            </p>
-        </header>
-    );
+  return (
+    <header className="pb-4 md:pb-6 md:pt-2 lg:pb-10 lg:pt-4">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Long Covid and Public Health
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        How public health interventions could decrease or increase the disease
+        burden of long covid
+      </p>
+    </header>
+  );
 }
