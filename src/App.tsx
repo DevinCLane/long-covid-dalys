70f4b27fa26916ec9718bd1c118c4e3e6f995e4f
Devin Lane <devin.c.lane@gmail.com>
2025-05-16 14:57:42 -0700

    :recycle: update app.tsx for renamed component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Header } from "@/components/header";
 import { SiteFooter } from "@/components/site-footer";
 import { WorkInProgressAlert } from "@/components/work-in-progress-alert";
-import { LCCases } from "./components/LC-cases";
+import { LCDALYs } from "@/components/LC-DALYs";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <WorkInProgressAlert />
       <Header />
       <div className="mx-auto flex w-full flex-col gap-8 lg:max-w-7xl">
-        <LCCases />
+        <LCDALYs />
         <SiteFooter />
       </div>
     </div>
