4ea937d7c60e2dd209a8b3ef2e0edb54e2381a8f
Devin Lane <devin.c.lane@gmail.com>
2025-05-19 17:02:36 -0700

    adjust vertical margins

--- src/components/work-in-progress-alert.tsx
+++ src/components/work-in-progress-alert.tsx
@@ -4,7 +4,7 @@ export function WorkInProgressAlert() {
   const date = new Date();
   const localDate = new Intl.DateTimeFormat(navigator.language).format(date);
   return (
-    <Alert className="self-center bg-accent lg:max-w-7xl">
+    <Alert className="self-center bg-accent">
       <AlertTitle>Heads up!</AlertTitle>
       <AlertDescription>
         This is a work in progress and currently displaying test data as of{" "}
