902faa5fe95d162b3ad45949d59c46b0695a6e39
Devin Lane <devin.c.lane@gmail.com>
2025-12-16 11:40:42 -0800

    scenario data refactor for reusability

--- src/config/scenarios.ts
+++ src/config/scenarios.ts
@@ -2,28 +2,35 @@ import DALYs from "@/data/DALYs.json";
 
 export interface Scenario {
   id: string;
-  label: string;
   DALYs: number;
   infected: number;
+  label: string;
   sublabel?: string;
   checked: boolean;
 }
 
+// 10th year is the final year of the model (in the 0 indexed list, 9 is the 10th item)
+const FINAL_YEAR = 9;
+
 // to do: read the data from the `DALYs.json`'
 // grab the id, the dalys, and the percentage of population infected
+
+/**
+ * Each scenario for which we are displays DALYs
+ */
 export const SCENARIOS: Scenario[] = [
   {
     // to do: each one of these (DALYs[1])[1] should be (DALYs[9])[1], (DALYs[9])[2], etc to represent the 10th year in the model
-    id: Object.keys(DALYs[1])[1],
-    DALYs: Object.values(DALYs[1])[1],
+    id: Object.keys(DALYs[FINAL_YEAR])[1],
+    DALYs: Object.values(DALYs[FINAL_YEAR])[1],
     infected: 29,
     label: "Long Covid Baseline",
     sublabel: "No air cleaning intervention.",
     checked: true,
   },
   {
-    id: Object.keys(DALYs[1])[2],
-    DALYs: Object.values(DALYs[1])[2],
+    id: Object.keys(DALYs[FINAL_YEAR])[2],
+    DALYs: Object.values(DALYs[FINAL_YEAR])[2],
     infected: 26,
     label: "HEPA in most common indoor spaces",
     sublabel:
@@ -31,8 +38,8 @@ export const SCENARIOS: Scenario[] = [
     checked: false,
   },
   {
-    id: Object.keys(DALYs[1])[3],
-    DALYs: Object.values(DALYs[1])[3],
+    id: Object.keys(DALYs[FINAL_YEAR])[3],
+    DALYs: Object.values(DALYs[FINAL_YEAR])[3],
     infected: 24,
     label: " HEPA schools and daycares",
     sublabel:
@@ -40,32 +47,32 @@ export const SCENARIOS: Scenario[] = [
     checked: false,
   },
   {
-    id: Object.keys(DALYs[1])[4],
-    DALYs: Object.values(DALYs[1])[4],
+    id: Object.keys(DALYs[FINAL_YEAR])[4],
+    DALYs: Object.values(DALYs[FINAL_YEAR])[4],
     infected: 11,
     label: "HEPA all public indoor air",
     sublabel: "HEPA filters implemented in all public indoor spaces.",
     checked: false,
   },
   {
-    id: Object.keys(DALYs[1])[5],
-    DALYs: Object.values(DALYs[1])[5],
+    id: Object.keys(DALYs[FINAL_YEAR])[5],
+    DALYs: Object.values(DALYs[FINAL_YEAR])[5],
     infected: 25,
     label: "Far UVC in most common indoor spaces",
     sublabel: "Far UVC in most common spaces indoor air (12% fewer cases).",
     checked: false,
   },
   {
-    id: Object.keys(DALYs[1])[6],
-    DALYs: Object.values(DALYs[1])[6],
+    id: Object.keys(DALYs[FINAL_YEAR])[6],
+    DALYs: Object.values(DALYs[FINAL_YEAR])[6],
     infected: 23,
     label: "Far UVC schools and daycares",
     sublabel: "Far UVC in schools and daycares.",
     checked: false,
   },
   {
-    id: Object.keys(DALYs[1])[7],
-    DALYs: Object.values(DALYs[1])[7],
+    id: Object.keys(DALYs[FINAL_YEAR])[7],
+    DALYs: Object.values(DALYs[FINAL_YEAR])[7],
     infected: 6,
     label: "Far UVC all public indoor air",
     sublabel: "Far UVC in all public indoor spaces.",
