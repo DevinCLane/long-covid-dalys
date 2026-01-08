f31e1a7fb206a5c6b989409bfa47f74440d83caf
Devin Lane <devin.c.lane@gmail.com>
2026-01-07 19:05:00 -0800

    scenario refactor - objects

--- src/config/scenarios.ts
+++ src/config/scenarios.ts
@@ -11,7 +11,7 @@ export interface Scenario {
 }
 
 // 10th year is the final year of the model (in the 0 indexed list, 9 is the 10th item)
-const FINAL_YEAR = 9;
+const FINAL_YEAR = DALYs.length - 1;
 
 export const getDefaultSelectedScenarios = (): Set<string> => {
   return new Set(
@@ -20,6 +20,8 @@ export const getDefaultSelectedScenarios = (): Set<string> => {
     ),
   );
 };
+
+const dalysEntries = Object.entries(DALYs[FINAL_YEAR]);
 
 // to do: read the data from the `DALYs.json`'
 // grab the id, the dalys, and the percentage of population infected
@@ -30,8 +32,8 @@ export const getDefaultSelectedScenarios = (): Set<string> => {
 export const SCENARIOS: Scenario[] = [
   {
     // to do: each one of these (DALYs[1])[1] should be (DALYs[9])[1], (DALYs[9])[2], etc to represent the 10th year in the model
-    id: Object.keys(DALYs[FINAL_YEAR])[1],
-    DALYs: Object.values(DALYs[FINAL_YEAR])[1],
+    id: dalysEntries[1][0],
+    DALYs: dalysEntries[1][1],
     infected: 29,
     group: "baseline",
     label: "Long Covid Baseline",
@@ -39,8 +41,8 @@ export const SCENARIOS: Scenario[] = [
     checked: true,
   },
   {
-    id: Object.keys(DALYs[FINAL_YEAR])[2],
-    DALYs: Object.values(DALYs[FINAL_YEAR])[2],
+    id: dalysEntries[2][0],
+    DALYs: dalysEntries[2][1],
     infected: 26,
     group: "HEPA",
     label: "HEPA in most common indoor spaces",
@@ -49,8 +51,8 @@ export const SCENARIOS: Scenario[] = [
     checked: false,
   },
   {
-    id: Object.keys(DALYs[FINAL_YEAR])[3],
-    DALYs: Object.values(DALYs[FINAL_YEAR])[3],
+    id: dalysEntries[3][0],
+    DALYs: dalysEntries[3][1],
     infected: 24,
     group: "HEPA",
     label: " HEPA schools and daycares",
@@ -59,8 +61,8 @@ export const SCENARIOS: Scenario[] = [
     checked: false,
   },
   {
-    id: Object.keys(DALYs[FINAL_YEAR])[4],
-    DALYs: Object.values(DALYs[FINAL_YEAR])[4],
+    id: dalysEntries[4][0],
+    DALYs: dalysEntries[4][1],
     infected: 11,
     group: "HEPA",
     label: "HEPA all public indoor air",
@@ -68,8 +70,8 @@ export const SCENARIOS: Scenario[] = [
     checked: false,
   },
   {
-    id: Object.keys(DALYs[FINAL_YEAR])[5],
-    DALYs: Object.values(DALYs[FINAL_YEAR])[5],
+    id: dalysEntries[5][0],
+    DALYs: dalysEntries[5][1],
     infected: 25,
     group: "UVC",
     label: "Far UVC in most common indoor spaces",
@@ -77,8 +79,8 @@ export const SCENARIOS: Scenario[] = [
     checked: false,
   },
   {
-    id: Object.keys(DALYs[FINAL_YEAR])[6],
-    DALYs: Object.values(DALYs[FINAL_YEAR])[6],
+    id: dalysEntries[6][0],
+    DALYs: dalysEntries[6][1],
     infected: 23,
     group: "UVC",
     label: "Far UVC schools and daycares",
@@ -86,8 +88,8 @@ export const SCENARIOS: Scenario[] = [
     checked: false,
   },
   {
-    id: Object.keys(DALYs[FINAL_YEAR])[7],
-    DALYs: Object.values(DALYs[FINAL_YEAR])[7],
+    id: dalysEntries[7][0],
+    DALYs: dalysEntries[7][1],
     infected: 6,
     group: "UVC",
     label: "Far UVC all public indoor air",
