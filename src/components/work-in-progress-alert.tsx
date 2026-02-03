1941455ac9dd8beb5dc3bd5b677842a774c6f2e5
Devin Lane <devin.c.lane@gmail.com>
2026-02-03 15:06:29 -0800

    upgrade alert component

--- src/components/work-in-progress-alert.tsx
+++ src/components/work-in-progress-alert.tsx
@@ -4,9 +4,9 @@ export function WorkInProgressAlert() {
   const date = new Date();
   const localDate = new Intl.DateTimeFormat(navigator.language).format(date);
   return (
-    <Alert className="mb-4 self-center bg-card">
+    <Alert className="bg-card mb-4 self-center">
       <AlertTitle>Heads up!</AlertTitle>
-      <AlertDescription>
+      <AlertDescription className="justify-items-center">
         This is a work in progress and currently displaying test data as of{" "}
         {localDate}
       </AlertDescription>
