124ef12ab29cb57ef42581920ab9b5afd967584c
Devin Lane <devin.c.lane@gmail.com>
2026-08-13 13:28:09 -0700

    title text for overview bar chart

--- src/components/charts/bar-chart.tsx
+++ src/components/charts/bar-chart.tsx
@@ -315,7 +315,8 @@ export function BarChartStacked({ onScenarioSelect }: BarChartStackedProps) {
       <CardHeader className="flex items-center gap-2 space-y-0 border-b sm:flex-row">
         <div className="grid flex-1 gap-1 text-center sm:text-left">
           <CardTitle className="text-l text-pretty md:text-2xl">
-            5-year DALYs for Acute COVID, Long COVID and PASC
+            How might air cleaning interventions affect COVID-associated
+            disability?
           </CardTitle>
           <CardDescription className="hidden md:block">
             <ChartDescriptionBody />
