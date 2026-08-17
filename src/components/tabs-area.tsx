70184d1766a20a3a31c389b9c28fa28619733d1c
Devin Lane <devin.c.lane@gmail.com>
2026-08-17 12:39:59 -0700

    change tabs pages for air cleaning, pharmaceuticals, public health, etc

--- src/components/tabs-area.tsx
+++ src/components/tabs-area.tsx
@@ -1,23 +1,9 @@
 import { useState } from "react";
-import { AcuteCovidChart } from "@/components/charts/acute-covid-chart";
-import { LongCovidChart } from "@/components/charts/long-covid-chart";
-import { PascChart } from "@/components/charts/pasc-chart";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
-import { getDefaultSelectedScenarios } from "@/config/scenarios";
 import { BarChartStacked } from "@/components/charts/bar-chart";
 import { DetailedBarChart } from "@/components/charts/detailed-bar-chart";
 
 export default function TabsArea() {
-  // selected scenarios for the year by year views
-  const [lcScenarios, setLcScenarios] = useState<Set<string>>(
-    getDefaultSelectedScenarios,
-  );
-  const [pascScenarios, setPascScenarios] = useState<Set<string>>(
-    getDefaultSelectedScenarios,
-  );
-  const [acuteScenarios, setAcuteScenarios] = useState<Set<string>>(
-    getDefaultSelectedScenarios,
-  );
   // active tabs for the detailed views
   const [activeTab, setActiveTab] = useState("overview");
   const [detailedScenarioId, setDetailedScenarioId] = useState("baseline");
@@ -56,23 +42,14 @@ export default function TabsArea() {
           onScenarioSelect={selectDetailedScenario}
         />
       </TabsContent>
-      <TabsContent value="lc" className="w-full">
-        <LongCovidChart
-          selectedScenarios={lcScenarios}
-          setSelectedScenarios={setLcScenarios}
-        />
+      <TabsContent value="air" className="w-full">
+        <BarChartStacked onScenarioSelect={openDetailedScenario} />
       </TabsContent>
-      <TabsContent value="pasc" className="w-full">
-        <PascChart
-          selectedScenarios={pascScenarios}
-          setSelectedScenarios={setPascScenarios}
-        />
+      <TabsContent value="pharmaceuticals" className="w-full">
+        <div>Coming soon</div>
       </TabsContent>
-      <TabsContent value="acute" className="w-full">
-        <AcuteCovidChart
-          selectedScenarios={acuteScenarios}
-          setSelectedScenarios={setAcuteScenarios}
-        />
+      <TabsContent value="publicHealth" className="w-full">
+        <div>Coming soon</div>
       </TabsContent>
     </Tabs>
   );
