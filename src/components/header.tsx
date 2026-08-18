9c80571429d5c7ce69a2f713802943ce79c4c6f2
Devin Lane <devin.c.lane@gmail.com>
2026-08-18 14:20:41 -0700

    change title of project

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,8 @@ export function Header() {
   return (
     <header className="pb-4 md:pt-2 md:pb-6 lg:pt-4 lg:pb-10">
       <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight md:text-4xl lg:text-5xl">
-        Long Covid DALYs Scenario Simulator
+        COVID-19 DALYs Scenario Simulator
       </h1>
-      {/* <p className="leading-7 not-first:mt-6">
-        Benefits of air cleaning interventions on COVID-19 infection and Long
-        COVID-related disability-adjusted life years: A policy simulation
-      </p> */}
     </header>
   );
 }
