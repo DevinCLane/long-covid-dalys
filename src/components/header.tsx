851e10238cf428fd740c0f0ccf070f018c9d9849
Devin Lane <devin.c.lane@gmail.com>
2026-08-19 09:53:33 -0700

    header text

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header className="pb-4 md:pt-2 md:pb-6 lg:pt-4 lg:pb-10">
       <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight md:text-4xl lg:text-5xl">
-        COVID-19 DALYs Scenario Simulator
+        COVID-19 DALYs Simulator
       </h1>
     </header>
   );
