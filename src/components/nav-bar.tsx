1052e354bd5fdb59d1c24fa56f39957311c8b4d3
Devin Lane <devin.c.lane@gmail.com>
2026-08-21 14:50:02 -0700

    remove github button from nav

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,11 +1,11 @@
-import { GitHub } from "@/components/github-button";
+// import { GitHub } from "@/components/github-button";
 // import { DarkLightToggle } from "@/components/dark-light-mode-toggle";
 import ShareButton from "@/components/share-button";
 
 export function NavBar() {
   return (
     <nav className="flex justify-end gap-2">
-      <GitHub />
+      {/* <GitHub /> */}
       <ShareButton />
       {/* <DarkLightToggle /> */}
     </nav>
