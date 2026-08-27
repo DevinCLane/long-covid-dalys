a1b153ee671107f06e5128d8ccbd98c57acfd94d
Devin Lane <devin.c.lane@gmail.com>
2026-08-27 10:03:36 -0700

    comment out scenario area (all these are for the dalys by year views which are no longer needed)

--- src/components/scenario-area.tsx
+++ src/components/scenario-area.tsx
@@ -1,47 +1,47 @@
-// takes in name for the scenario
-// checkbox to click
-// logic for what happens to checkbox?
+// // takes in name for the scenario
+// // checkbox to click
+// // logic for what happens to checkbox?
 
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
-import type { Scenario } from "@/config/scenarios";
+// import { Checkbox } from "@/components/ui/checkbox";
+// import { Label } from "@/components/ui/label";
+// import type { Scenario } from "@/config/scenarios";
 
-interface ScenarioAreaProps extends Scenario {
-  onCheckedChange: (checked: boolean) => void;
-}
+// interface ScenarioAreaProps extends Scenario {
+//   onCheckedChange: (checked: boolean) => void;
+// }
 
-export function ScenarioArea({
-  id,
-  label,
-  dalys,
-  infected,
-  sublabel,
-  checked,
-  onCheckedChange,
-}: ScenarioAreaProps) {
-  return (
-    <div className="m-4 flex flex-col gap-6">
-      <div className="flex flex-col items-start gap-3">
-        <div className="flex gap-4">
-          <Checkbox
-            id={id}
-            checked={checked}
-            onCheckedChange={onCheckedChange}
-          />
-          <Label htmlFor={id}>{label}</Label>
-        </div>
-        <div className="text-sm">
-          {dalys} DALYs per 1000 people over 10 years
-        </div>
-        <div className="text-sm">~{infected}% infected annually</div>
-        <div>
-          {sublabel && (
-            <p className="text-muted-foreground text-start text-sm">
-              {sublabel}
-            </p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-}
+// export function ScenarioArea({
+//   id,
+//   label,
+//   dalys,
+//   infected,
+//   sublabel,
+//   checked,
+//   onCheckedChange,
+// }: ScenarioAreaProps) {
+//   return (
+//     <div className="m-4 flex flex-col gap-6">
+//       <div className="flex flex-col items-start gap-3">
+//         <div className="flex gap-4">
+//           <Checkbox
+//             id={id}
+//             checked={checked}
+//             onCheckedChange={onCheckedChange}
+//           />
+//           <Label htmlFor={id}>{label}</Label>
+//         </div>
+//         <div className="text-sm">
+//           {dalys} DALYs per 1000 people over 10 years
+//         </div>
+//         <div className="text-sm">~{infected}% infected annually</div>
+//         <div>
+//           {sublabel && (
+//             <p className="text-muted-foreground text-start text-sm">
+//               {sublabel}
+//             </p>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
